d2fe7de324c65fd41de8071ae711f7124b5cc466
Akash Jain <40735525+mr-jain@users.noreply.github.com>
2018-07-11 19:30:08 +0530

    Add files via upload

---
+++ day 9/Bubblesrt.js
@@ -0,0 +1,31 @@
+var arr = [9,2,4,11,33,10,6];
+var temp;
+function BubbleSort()
+{
+   for(var i = 0 ; i<arr.length-1; i++)
+   { 
+      var swap = false;
+      for(var j = 0; j<arr.length-i-1; j++)
+      { 
+         if(arr[j]>arr[j+1])
+         {
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1]  = temp;
+            swap = true;
+         }
+      }
+   }
+   
+   if(swap == false)
+   {
+      for(var i = 0; i<arr.length;i++)
+      {
+         console.log(arr[i]);  
+      }
+   }
+
+
+}
+
+BubbleSort();
