1e1b94dcde12ac147a9e86653a8944b2d5da323c
Akash Jain <40735525+mr-jain@users.noreply.github.com>
2018-07-27 21:34:01 +0530

    Add files via upload

---
+++ Day25/index.js
@@ -0,0 +1,15 @@
+console.log("starting app");
+
+const _ = require('lodash');
+
+console.log(_.isString(true));
+console.log(_.isString('akash'));
+console.log(_.isDate(new Date));
+console.log(_.multiply(11 , 34));
+console.log(_.truncate('hi-diddly-ho there, neighborino', {
+  'length': 24,
+  'separator': ' '
+}));
+
+var filterarray= _.uniq(['akash',1,'akash',1,2,3,4]);
+console.log(filterarray);  
