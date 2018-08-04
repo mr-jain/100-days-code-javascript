69f7b0c52ceac53dd280a3578a87e349b5f5e599
Akash Jain <40735525+mr-jain@users.noreply.github.com>
2018-08-04 20:46:11 +0530

    Create lgcl.c

---
+++ DAY 32/lgcl.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d",printf("%d",printf("%d",printf("%d",printf("%d",printf("%s","Hello World"))))));
+
+    return 0;
+}
+
+//output
+Hello World112111
