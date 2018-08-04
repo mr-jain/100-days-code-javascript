84305d6c76932d0e6d321fd545606ec7ac5719dd
Akash Jain <40735525+mr-jain@users.noreply.github.com>
2018-08-04 21:04:05 +0530

    Create lgcl7.c

---
+++ DAY 32/lgcl7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    main();
+}
+// segmentation fault
+//it occurs when a program attempts to access a memory locations that is not allowed so it retruns segmentation fault
