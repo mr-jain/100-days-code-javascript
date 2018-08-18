b68741a4e4e7068c5d959600601ebed7cd3324d4
Akash Jain <40735525+mr-jain@users.noreply.github.com>
2018-08-18 17:02:04 +0530

    Create lgcl 2.java

---
+++ DAY 42/lgcl 2.java
@@ -0,0 +1,16 @@
+public class test
+{		
+	public static void main(String[] args) 
+	{
+		String x = null;
+		x=givemestring(x);
+		System.out.println(x);
+    }
+	static String givemestring(String y)
+	{
+		         y =  "test";
+		         return y;
+	}
+}
+
+//output test
