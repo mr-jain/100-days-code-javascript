6465eafe78b4d243036c08387fde05cebb2a370e
Akash Jain <40735525+mr-jain@users.noreply.github.com>
2018-08-18 17:05:35 +0530

    Create lgcl.java

---
+++ DAY 43/lgcl.java
@@ -0,0 +1,10 @@
+public class exception_handling 
+{		
+	public static void main(String[] args) 
+	{
+        Thread t = Thread.currentThread();
+        System.out.println(t);
+	}	
+}
+
+//Thread[main , 5 ,main]
