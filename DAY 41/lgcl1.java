0eb17430ca0e10b54b28072b21a306e83eaaaa25
Akash Jain <40735525+mr-jain@users.noreply.github.com>
2018-08-18 16:42:22 +0530

    Create lgcl1.java

---
+++ DAY 41/lgcl1.java
@@ -0,0 +1,15 @@
+public class mainclass 
+{		
+	public static void main(String[] args) 
+	{	        		   
+            box  obj = new box();
+            System.out.println(obj);
+    }
+}
+class box 
+{
+   int weight;
+   int height;
+}
+
+//output garbage value
